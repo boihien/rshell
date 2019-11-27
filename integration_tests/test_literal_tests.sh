b5265f006af289c37b260a5245482e4f333a60d9
Boi-Hien Huang <bhuan019@ucr.edu>
2019-11-26 22:34:30 -0800

    finished integration tests

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 test -e /home/csmajs/bhuan019/assignment-memoryleak/src/main.cpp && echo "main.cpp exists"
-
 test -f /home/csmajs/bhuan019/assignment-memoryleak/src/command.cpp && echo "command.cpp exists"
-
 test -d /home/csmajs/bhuan019/assignment-memoryleak/src && echo "src exists"
+test /home/csmajs/bhuan019/assignment-memoryleak/src/ && echo "src is there"
+(test -e /home/csmajs/bhuan019/assignment-memoryleak/src/main.cpp) && (mkdir hello && echo "finish")
+
 
 exit
 
--- integration_tests/test_symbolic_test.sh
+++ integration_tests/test_symbolic_test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-test -e /home/csmajs/bhuan019/src
+[-e /home/csmajs/bhuan019/assignment-memoryleak/src/main.cpp && echo "main.cpp exists"]
+[-f /home/csmajs/bhuan019/assignment-memoryleak/src/command.cpp && echo "command.cpp exists"]
+[-d /home/csmajs/bhuan019/assignment-memoryleak/src && echo "src exists"]
+([test /home/csmajs/bhuan019/assignment-memoryleak/src/main.cpp] && mkdir new) && echo "new"
+[test /home/csmajs/bhuan019/assignment-memoryleak/src/]
+
+exit
