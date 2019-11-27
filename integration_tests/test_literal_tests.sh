4a0d090e098811382918924ed74befcb3da48837
Boi-Hien Huang <bhuan019@ucr.edu>
2019-11-26 22:26:09 -0800

    it

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 test -e /home/csmajs/bhuan019/assignment-memoryleak/src/main.cpp && echo "main.cpp exists"
+
+test -f /home/csmajs/bhuan019/assignment-memoryleak/src/command.cpp && echo "command.cpp exists"
+
+test -d /home/csmajs/bhuan019/assignment-memoryleak/src && echo "src exists"
+
+exit
+
